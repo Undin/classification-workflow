5da30fbe8bba7679b3be31bf87810306d5141725
Undin <a.pendryak@yandex.ru>
2016-08-28 18:38:19 +0300

    added 'name' property to Algorithm class

--- core/src/main/kotlin/com/warrior/classification/workflow/core/Algorithm.kt
+++ core/src/main/kotlin/com/warrior/classification/workflow/core/Algorithm.kt
@@ -10,28 +10,26 @@ import java.io.Serializable
 /**
  * Created by warrior on 27/08/16.
  */
-sealed class Algorithm : Serializable {
+sealed class Algorithm(val name: String) : Serializable {
     class Classifier @JsonCreator constructor(
+            @JsonProperty("name") name: String,
             @JsonProperty("classifier_class") val classifierClass: String,
             @JsonProperty("classifier_options") val options: List<String>
-    ) : Algorithm() {
+    ) : Algorithm(name) {
         operator fun invoke(): weka.classifiers.Classifier = AbstractClassifier.forName(classifierClass, options.toTypedArray())
-        override fun toString(): String{
-            return "Classifier(classifierClass='$classifierClass', options=$options)"
-        }
     }
     class Transformer @JsonCreator constructor(
+            @JsonProperty("name") name: String,
             @JsonProperty("search_class") val searchClass: String,
             @JsonProperty("search_options") val searchOptions: List<String>,
             @JsonProperty("evaluation_class") val evaluationClass: String,
             @JsonProperty("evaluation_options") val evaluationOptions: List<String>
-    ) : Algorithm() {
+    ) : Algorithm(name) {
         operator fun invoke(): Pair<ASSearch, ASEvaluation> = Pair(
                 ASSearch.forName(searchClass, searchOptions.toTypedArray()),
                 ASEvaluation.forName(evaluationClass, evaluationOptions.toTypedArray())
         )
-        override fun toString(): String{
-            return "Transformer(searchClass='$searchClass', searchOptions=$searchOptions, evaluationClass='$evaluationClass', evaluationOptions=$evaluationOptions)"
-        }
     }
+
+    override fun toString(): String = name
 }

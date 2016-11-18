d1a83ca2e476b0ec567584d0144a2ab277b31439
Undin <a.pendryak@yandex.ru>
2016-11-18 22:55:18 +0300

    remove useless attributes while loading dataset

--- core/src/main/kotlin/com/warrior/classification_workflow/core/utils.kt
+++ core/src/main/kotlin/com/warrior/classification_workflow/core/utils.kt
@@ -4,15 +4,39 @@ import weka.core.Instances
 import weka.core.converters.ArffSaver
 import weka.core.converters.ConverterUtils
 import weka.core.converters.Saver
+import weka.filters.Filter
+import weka.filters.unsupervised.attribute.Remove
 import java.io.File
+import java.util.*
 
 /**
  * Created by warrior on 12/07/16.
  */
-fun load(path: String): Instances {
+fun load(path: String, removeUseless: Boolean = true): Instances {
     val instances = ConverterUtils.DataSource.read(path)
     instances.setClassIndex(instances.numAttributes() - 1)
-    return instances
+    return if (removeUseless) removeUseless(instances) else instances
+}
+
+fun removeUseless(instances: Instances): Instances {
+    val firstInstance = instances[0]
+    val uselessAttributes = ArrayList<Int>()
+    for (attr in instances.enumerateAttributes()) {
+        if (attr.index() != instances.classIndex()) {
+            val isConstAttr = instances.none { it.value(attr) != firstInstance.value(attr) }
+            if (isConstAttr) {
+                uselessAttributes += attr.index()
+            }
+        }
+    }
+    return if (uselessAttributes.isNotEmpty()) {
+        val remove = Remove()
+        remove.setAttributeIndicesArray(uselessAttributes.toIntArray())
+        remove.setInputFormat(instances)
+        Filter.useFilter(instances, remove)
+    } else {
+        instances
+    }
 }
 
 fun save(dataSet: Instances, dst: String, saver: Saver = ArffSaver()) {

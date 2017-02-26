3e2f9ae46204f02bd92753c8f40db25a55a7b401
Arseniy Pendryak <a.pendryak@yandex.ru>
2017-02-26 18:10:52 +0800

    use 'parseArgs' from utils

--- master/src/main/kotlin/com/warrior/classification_workflow/main.kt
+++ master/src/main/kotlin/com/warrior/classification_workflow/main.kt
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory
 import com.fasterxml.jackson.module.kotlin.readValue
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
-import org.apache.commons.cli.*
 import java.io.File
 
 /**
@@ -17,59 +16,4 @@ fun main(args: Array<String>) {
 
     val constructor = WorkflowConstructor(config)
     constructor.construct(config.dataset)
-}
-
-private fun parseArgs(args: Array<String>): String? {
-    val configOption = Option.builder("c")
-            .longOpt("config")
-            .hasArg(true)
-            .argName("path")
-            .desc("path to config_name.yaml file")
-            .build()
-    val helpOption = Option.builder("h")
-            .longOpt("help")
-            .desc("show this help")
-            .build()
-    val allOptions = options(configOption, helpOption)
-
-    val parser = DefaultParser()
-    val line = try {
-        parser.parse(options(helpOption), args, false)
-    } catch (e: ParseException) {
-        null
-    }
-    if (line != null && line.hasOption(helpOption.opt)) {
-        printHelp(allOptions)
-        System.exit(0)
-    } else {
-        try {
-            val configOptions = options(configOption)
-            val line = parser.parse(configOptions, args)
-            if (line.hasOption(configOption.opt)) {
-                return line.getOptionValue(configOption.opt)
-            } else {
-                printHelp(allOptions)
-                System.exit(0)
-            }
-        } catch (e: ParseException) {
-            e.printStackTrace(System.err)
-            printHelp(allOptions)
-            System.exit(1)
-        }
-    }
-    // unreachable
-    return ""
-}
-
-private fun options(vararg opts: Option): Options {
-    val options = Options()
-    for (opt in opts) {
-        options.addOption(opt)
-    }
-    return options
-}
-
-private fun printHelp(options: Options) {
-    val formatter = HelpFormatter()
-    formatter.printHelp("java -jar jarfile.jar [options...]", options)
 }

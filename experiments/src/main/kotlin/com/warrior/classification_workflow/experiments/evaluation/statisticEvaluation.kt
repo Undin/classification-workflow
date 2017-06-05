4daae2af67c1dc2743cb5f1122b2157e90875b32
Arseniy Pendryak <a.pendryak@yandex.ru>
2017-06-05 20:58:18 +0300

    fix statistic evaluation

--- experiments/src/main/kotlin/com/warrior/classification_workflow/experiments/evaluation/statisticEvaluation.kt
+++ experiments/src/main/kotlin/com/warrior/classification_workflow/experiments/evaluation/statisticEvaluation.kt
@@ -5,7 +5,10 @@ import com.warrior.classification_workflow.WorkflowConstructor
 fun main(args: Array<String>) {
     for (i in 0 until 10) {
         evaluation(args) { config ->
-            val iterConfig = config.copy(logFolder = "${config.logFolder}-$i", version = "${config.version}-$i")
+            val iterConfig = config.copy(
+                    outFolder = "${config.outFolder}-$i",
+                    logFolder = "${config.logFolder}-$i",
+                    version = "${config.version}-$i")
             WorkflowConstructor(iterConfig)
         }
     }
